96653597a7bd509fdf1386f58be0a704ed5f1b4d
USDT2030 <fadifadi203074@gmail.com>
2025-10-04 01:29:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
